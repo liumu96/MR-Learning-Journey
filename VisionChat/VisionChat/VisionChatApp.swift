cb918b734611ec9b14eb2555c18d06f890958504
liumu96 <918883717@qq.com>
2024-08-16 13:48:59 +0800

    Vision Chat

---
+++ VisionChat/VisionChat/VisionChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionChatApp.swift
+//  VisionChat
+//
+//  Created by liuxing on 2024/8/16.
+//
+
+import SwiftUI
+
+@main
+struct VisionChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
